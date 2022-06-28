137921a8709114be903ce32e53701d0c4c76883c
Sergey <canuseethelight@mail.ru>
2022-06-28 18:22:51 +0300

    added Condition

---
+++ Desktop/examples/Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name ");
+string username = Console.ReadLine();
+
+if (username.ToLower()== "oleg")
+{
+    Console.WriteLine("Ura, this is Oleg");
+}
+else
+{
+    Console.Write("hello, ");
+    Console.Write(username);
+}
